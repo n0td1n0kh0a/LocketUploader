c969e279ce2182d5c6054fef2d7d9efba75c4e1d
n0td1n0kh0a <152876934+n0td1n0kh0a@users.noreply.github.com>
2024-11-08 23:41:07 +0700

    make it work with dino's backend

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,8 +1,8 @@
 const constants = {
     apiRoutes: {
-        LOGIN_URL: "https://locketuploader-be-render.onrender.com/locket/login",
+        LOGIN_URL: "https://locket-backend.dinokhoa.online/locket/login",
         UPLOAD_MEDIA_URL:
-            "https://locketuploader-be-render.onrender.com/locket/upload-media",
+            "https://locket-backend.dinokhoa.online/locket/upload-media",
     },
     toastSettings: {
         position: "bottom-right",
